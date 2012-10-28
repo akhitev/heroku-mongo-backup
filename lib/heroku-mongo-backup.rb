1de7a6e4fceae03cee83014e5549405efa710c27
Alexander Khitev <alexander.khitev@gmail.com>
2012-10-29 03:23:55 +0400

    added demo environment

--- lib/heroku-mongo-backup.rb
+++ lib/heroku-mongo-backup.rb
@@ -152,7 +152,7 @@ module HerokuMongoBackup
     def initialize connect = true
       @file_name = Time.now.strftime("%Y-%m-%d_%H-%M-%S.gz")
   
-      if( ['production', 'staging'].include?(ENV['RAILS_ENV'] || ENV['RACK_ENV']) )
+      if( ['production', 'staging','demo'].include?(ENV['RAILS_ENV'] || ENV['RACK_ENV']) )
 
         #config_template = ERB.new(IO.read("config/mongoid.yml"))
         #uri = YAML.load(config_template.result)['production']['uri']
